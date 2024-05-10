6bddabc8d9906c22c588067989825b7c3e751340
YasRibeiroo <133972398+YasRibeiroo@users.noreply.github.com>
2024-05-10 02:13:09 -0300

    Organizando List e Array na Classe Listar.Produto.java, com a criação de um método para realistar a listagem dos produtos

--- src/src/CadastroDeProduto.java
+++ src/src/CadastroDeProduto.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class CadastroDeProduto {
+public class CadastroDeProduto {
+
 }
--- src/src/ListarProdutos.java
+++ src/src/ListarProdutos.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ListarProdutos {
+//Criando classe de listagem de produtos
+public class ListarProdutos{
+
+    //usando list
+    private List<Produto> produtos;
+
+    //usando array
+    public ListarProdutos() {
+        produtos = new ArrayList<>();
+    }
+    //metodo de listagem
+    public void listagemProdutos() {
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
+    }
 }
