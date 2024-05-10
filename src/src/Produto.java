47a9e05e2b4b3dc1a50370855763f10f762bd718
YasRibeiroo <133972398+YasRibeiroo@users.noreply.github.com>
2024-05-10 02:03:36 -0300

    Declarando variáveis e organizando Get na classe Produto.java

--- src/src/Produto.java
+++ src/src/Produto.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Produto {
+//criando a classe produto
+public class Produto {
+    //declarando variáveis de nome e preço
+    private String nome;
+    private double preco;
+
+    //Get
+    public Produto( String nome, double preco) {
+
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return  " Nome: " + nome + ", Preço: R$" + preco;
+    }
 }
+
+
