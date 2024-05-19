468067ddaa35ed28485dc592621b7e790188979f
YasRibeiroo <133972398+YasRibeiroo@users.noreply.github.com>
2024-05-19 17:09:02 -0300

    criando classe para deletar

--- src/src/DeletarProdutos.java
+++ src/src/DeletarProdutos.java
@@ -1,2 +1,29 @@
-public class DeletarProdutos {
+import java.util.Iterator;
+import java.util.List;
+
+public class DeletarProdutos { private ListarProdutos listarProdutos;
+
+    public DeletarProdutos(ListarProdutos listarProdutos) {
+        this.listarProdutos = listarProdutos;
+    }
+
+    public void deletarProdutoPorNome(String nome) {
+        List<Produto> produtos = listarProdutos.getProdutos();
+        boolean produtoEncontrado = false;
+
+        for (Iterator<Produto> iterator = produtos.iterator(); iterator.hasNext();) {
+            Produto produto = iterator.next();
+            if (produto.getNome().equalsIgnoreCase(nome)) {
+                iterator.remove();
+                produtoEncontrado = true;
+                System.out.println("Produto deletado com sucesso: " + nome);
+                break;
+            }
+        }
+
+        if (!produtoEncontrado) {
+            System.out.println("Produto não encontrado: " + nome);
+        }
+    }
 }
+
