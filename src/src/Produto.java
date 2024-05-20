3915c14f719f2e603a54716e893783a45c2007a5
Miguel - Rodrigues <143560179+MiguelRodriguesCRN@users.noreply.github.com>
2024-05-20 11:15:26 -0300

    FINALIZAÇÃO PROJETO JAVA P2º REQUISTOS FINALIZADOS

--- src/src/Produto.java
+++ src/src/Produto.java
@@ -1,36 +1,43 @@
-public class Produto {
-
-    private String nome;
+public class Produto extends Item {
     private double preco;
     private double porcentagemLucro;
-    private int quantidade;
 
     public Produto(String nome, double preco, double porcentagemLucro, int quantidade) {
-        this.nome = nome;
+        super(nome, quantidade);
         this.preco = preco;
         this.porcentagemLucro = porcentagemLucro;
-        this.quantidade = quantidade;
-    }
-
-    public String getNome() {
-        return nome;
     }
 
     public double getPreco() {
         return preco;
     }
 
+    public void setPreco(double preco) {
+        if (preco < 0) {
+            throw new IllegalArgumentException("O preço não pode ser negativo.");
+        }
+        this.preco = preco;
+    }
+
     public double getPorcentagemLucro() {
         return porcentagemLucro;
     }
 
-    public int getQuantidade() {
-        return quantidade;
+    public void setPorcentagemLucro(double porcentagemLucro) {
+        if (porcentagemLucro < 0 || porcentagemLucro > 100) {
+            throw new IllegalArgumentException("A porcentagem de lucro deve estar entre 0 e 100.");
+        }
+        this.porcentagemLucro = porcentagemLucro;
+    }
+
+    @Override
+    public double calcularPrecoVenda() {
+        return getPreco() + (getPreco() * getPorcentagemLucro() / 100);
     }
 
     @Override
     public String toString() {
-        return String.format("Nome: %s| Preço: %.2f| Porcentagem de Lucro: %.2f%%| Quantidade: %d",
-                nome, preco, porcentagemLucro, quantidade);
+        return String.format("Nome: %s | Preço de Custo: %.2f | Porcentagem de Lucro: %.2f%% | Preço de Venda: %.2f | Quantidade: %d",
+                getNome(), getPreco(), getPorcentagemLucro(), calcularPrecoVenda(), getQuantidade());
     }
 }
